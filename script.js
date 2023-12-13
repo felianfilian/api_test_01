2a8420a1f4f0515d76f9df83ecf1d52ce185a220
Dennis Schmalstieg <darianbellentor@outlook.de>
2023-12-14 00:09:19 +0100

    first test

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
-alert("hello");
+let output = document.getElementById("output");
+let data = fetch("https://pokeapi.co/api/v2/pokemon/ditto");
+
+output.innerHTML = "Testdata:<br>";
+output.innerHTML += data;
